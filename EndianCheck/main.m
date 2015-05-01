dce142d58ea932318a9e04444ed8b25ca87069cc
Calvin Cheng <linchuan.cheng@gmail.com>
2015-05-01 09:44:06 +0800

    Getting the endian-ness.

--- EndianCheck/main.m
+++ EndianCheck/main.m
@@ -7,11 +7,20 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "EndianCheck.h"
 
 int main(int argc, const char * argv[]) {
+    
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        EndianCheck *endian = [[EndianCheck alloc] init];
+        NSUInteger endianType = [endian byteOrder];
+        NSLog(@"ENDIAN_UNKNOWN is %lu", ENDIAN_UNKNOWN);
+        NSLog(@"ENDIAN_LITTLE is %lu", ENDIAN_LITTLE);
+        NSLog(@"ENDIAN_BIG is %lu", ENDIAN_BIG);
+        
+        NSLog(@"Our current device has endian %lu", (unsigned long)endianType);
     }
     return 0;
 }
